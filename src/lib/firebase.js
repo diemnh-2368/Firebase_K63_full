1c5a3cfd5997a6d850a01f951b3d0e546920e7f7
someseconds1402 <>
2022-06-23 08:28:29 +0000

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwgq1B0SI4n4h8M9MGQwayDAGqB9ElTU0",
+  authDomain: "long-nh-20184139-itss.firebaseapp.com",
+  projectId: "long-nh-20184139-itss",
+  storageBucket: "long-nh-20184139-itss.appspot.com",
+  messagingSenderId: "86228182582",
+  appId: "1:86228182582:web:29ceae2cd23685c52d1a97"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+
+// import { initializeApp } from "firebase/app";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBwgq1B0SI4n4h8M9MGQwayDAGqB9ElTU0",
+//   authDomain: "long-nh-20184139-itss.firebaseapp.com",
+//   projectId: "long-nh-20184139-itss",
+//   storageBucket: "long-nh-20184139-itss.appspot.com",
+//   messagingSenderId: "86228182582",
+//   appId: "1:86228182582:web:29ceae2cd23685c52d1a97"
+// };
+
+// const app = initializeApp(firebaseConfig);
