5f554a92c57acdadd46d0dfbe7b6df5a0365b4d9
tiendattrinh <>
2022-06-26 14:32:51 +0000

    Use Firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKjQAEMWqgzpfLUjMedK_846dxDgEgdN0",
-  authDomain: "fir-k63-trinhtiendat.firebaseapp.com",
-  projectId: "fir-k63-trinhtiendat",
-  storageBucket: "fir-k63-trinhtiendat.appspot.com",
-  messagingSenderId: "364551147633",
-  appId: "1:364551147633:web:6819b4f3705708f0e5cbd0",
-  measurementId: "G-FFRC78MKFJ"
+  apiKey: "AIzaSyA-gLFrI7yDV59zTHgro7P6FPo63Xa3VV4",
+  authDomain: "itss-trinhdat-20184065.firebaseapp.com",
+  projectId: "itss-trinhdat-20184065",
+  storageBucket: "itss-trinhdat-20184065.appspot.com",
+  messagingSenderId: "860296152862",
+  appId: "1:860296152862:web:489cfaccd3e2f254852b0f",
+  measurementId: "G-D29M52EGG3"
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
