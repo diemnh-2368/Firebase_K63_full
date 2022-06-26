7407140b313788733ef5ee8ee0cb4034cd44cb9e
H-HauDV <doanhau1407@gmail.com>
2022-06-26 16:56:51 +0700

    Use firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCttQgWJlKOqMiLroiUu96Ll-4OXF1qhX4",
+  authDomain: "todo-app-1-2022-itssinjapanese.firebaseapp.com",
+  projectId: "todo-app-1-2022-itssinjapanese",
+  storageBucket: "todo-app-1-2022-itssinjapanese.appspot.com",
+  messagingSenderId: "400382809474",
+  appId: "1:400382809474:web:92718a3fefc1c5d65dab9b",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
